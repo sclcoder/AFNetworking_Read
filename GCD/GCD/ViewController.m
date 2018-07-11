8f99cb65aec30428904c41d60bd5623516439483
scl <sclcoder@163.com>
2018-07-12 01:36:32 +0800

    GCD中基本概念的理解

--- GCD/GCD/ViewController.m
+++ GCD/GCD/ViewController.m
@@ -16,12 +16,113 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
     
-    [self test4];
+    [self testMain_Queue_Async];
+    
 }
 
-// MARK:<测试dispatch_sync在并发队列、串行队列中运行场景>
-// 并发队列
+
+// MARK:<主队列:特殊的串行队列>
+
+/***************** 主队列 *******************/
+/*!
+ * @function dispatch_get_main_queue
+ *
+ * @abstract
+ * Returns the default queue that is bound to the main thread.
+ *
+ * @discussion
+ * In order to invoke blocks submitted to the main queue, the application must
+ * call dispatch_main(), NSApplicationMain(), or use a CFRunLoop on the main
+ * thread.
+ *
+ * @result
+ * Returns the main queue. This queue is created automatically on behalf of
+ * the main thread before main() is called.
+ */
+
+- (void)testMain_Queue_Sync{
+    // 死锁
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSLog(@"aaaaa");
+    }); // 此处的整个blcok块为'任务a'即^{NSLog(@"aaaaa");}
+}
+/**
+    首先testMain_Queue_Sync{...}这块内容为'test任务'--包含'{''}'
+ 死锁原因: 主队列开始执行'test任务'---'任务a'进入主队列---因为是dispatch_sync()函数,必须要执行完‘任务a’才能返回,所以要执行'任务a'---因为主队类属于串行队列,必须执行完当前的'test任务'(最后的'}'算执行完成)才能调度队列中的'任务a'--所以死锁是因为主队列无法调用'任务a'导致dispatch_sync函数无法返回。
+
+ **/
+
+- (void)testMain_Queue_Async{
+    
+    /// '{' 之间的都是test任务 '}' 包含'{' '}'
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"aaaaa-%@",[NSThread currentThread]);
+    }); // 此处的整个blcok块为任务a ^{...}
+    
+    sleep(1);
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSLog(@"bbbbb-%@",[NSThread currentThread]);
+    });// 此处的整个blcok块为任务b ^{...}
+
+    NSLog(@"11111");
+    
+    /** 主队列属于串行队列---特点:主队列上的任务都在主线程中执行
+        首先整个testMain_Queue_Async方法当做任务进入到主队列中,这里把该任务称作'test任务'。'test任务'开始执行后主线程中的具体情况如下
+     任务a进入主队列--执行sleep--任务b进入主队列--输入11111(直到读取到方法的'}'这个'test任务'才算执行完成)--'test任务'完成后主队列调度任务a--主队列调度任务b。执行顺序符合串行队列的原则。
+     **/
+
+//    2018-07-12 01:02:56.939857+0800 GCD[56050:941275] 11111
+//    2018-07-12 01:02:58.049443+0800 GCD[56050:941275] aaaaa-<NSThread: 0x60000006ae00>{number = 1, name = main}
+//    2018-07-12 01:02:58.049712+0800 GCD[56050:941275] bbbbb-<NSThread: 0x60000006ae00>{number = 1, name = main}
+
+}
+
+/***************** 主队列 *******************/
+
+
+// MARK:<dispatch_sync()同步任务--并行、串行>
+/**
+ * @abstract
+ * Submits a block for synchronous execution on a dispatch queue.
+ *
+ * @discussion
+ * Submits a block to a dispatch queue like dispatch_async(), however
+ * dispatch_sync() will not return until the block has finished.
+   dispatch_sync()函数直到block完成才返回
+ *
+ * Calls to dispatch_sync() targeting the current queue will result
+ * in dead-lock. Use of dispatch_sync() is also subject to the same
+ * multi-party dead-lock problems that may result from the use of a mutex.
+ * Use of dispatch_async() is preferred.
+ 
+ 从测试结果来看 并不是在当前队列中调用dispatch_sync()都会造成死锁,只有在当前队列为串行队列时调用dispatch_sync()才会发生死锁。如果当前队列为并发队列调用dispatch_sync()不会发生死锁
+ 
+ * Unlike dispatch_async(), no retain is performed on the target queue. Because
+ * calls to this function are synchronous, the dispatch_sync() "borrows" the
+ * reference of the caller.
+ *
+ * As an optimization, dispatch_sync() invokes the block on the current
+ * thread when possible.
+   为了优化,dispatch_sync()函数执行任务的时候回尽量在当前线程中执行
+ 
+ *
+ * @param queue
+ * The target dispatch queue to which the block is submitted.
+ * The result of passing NULL in this parameter is undefined.
+ *
+ * @param block
+ * The block to be invoked on the target dispatch queue.
+ * The result of passing NULL in this parameter is undefined.
+ 
+   dispatch_sync(dispatch_queue_t queue, DISPATCH_NOESCAPE dispatch_block_t block);
+ */
+
+/**************************** dispatch_sync()中的死锁 **********************/
+// 当前队列为主队列的情况下-并发队列queueA中执行同步任务
 - (void)test1{
     
     dispatch_queue_t queue = dispatch_queue_create("com.yunjifen.concurrent", DISPATCH_QUEUE_CONCURRENT);
@@ -42,8 +143,12 @@
     
     NSLog(@"4444444--%@",[NSThread currentThread]);
     
-    /// dispatch_sync函数将任务加入队列后需要等待任务完成后才能返回
-    /// dispatch_sync会阻塞当前queue而不是阻塞当前线程，执行1的时候是在主线程上执行（官方文档：为了优化dispatch_sync会在当前线程中执行任务）
+    /**
+        dispatch_sync函数将任务加入队列后需要等待任务完成后才能返回
+        dispatch_sync会阻塞当前queue而不是阻塞当前线程？？,
+        执行1的为什么是在主线程上执行？--（API官方：* As an optimization, dispatch_sync() invokes the block on the current thread when possible.)
+     **/
+    
 
 //    2018-07-11 17:16:00.147262+0800 GCD[2923:1250266] 1111111111--<NSThread: 0x1c407e4c0>{number = 1, name = main}
 //    2018-07-11 17:16:05.148652+0800 GCD[2923:1250266] 222222222--<NSThread: 0x1c407e4c0>{number = 1, name = main}
@@ -52,11 +157,13 @@
     
 }
 
+
 - (void)test2{
     
     dispatch_queue_t queue = dispatch_queue_create("com.yunjifen.concurrent", DISPATCH_QUEUE_CONCURRENT);
     
     dispatch_sync(queue, ^{ // blockA
+        
         NSLog(@"000000000--%@",[NSThread currentThread]);
 
         dispatch_sync(queue, ^{ // blockB
@@ -65,14 +172,23 @@
         
         NSLog(@"222222222--%@",[NSThread currentThread]);
     });
-    
+
     NSLog(@"333333333--%@",[NSThread currentThread]);
     
-    // blockA进入队列queue中,因为是同步任务，要执行blockA后才能返回,所以执行blockA。遇到blockB，blockB进入队列queue中，又是同步任务，要执行blockB后才能返回,但是这时候blockA还没完成。
-    // 为什么不发生死锁呢？这要是串行队列早就死锁了。记录我的想法：因为是并发队列,blockA没完成就垃圾霸道，我让blockB出队列,完成blockB就好了。
-    // 如果是串行队列呢？在执行blockA时候遇到了blockB进入队列。因为是串行队列我必须把blockA执行完了才能调用队列中的blockB，但是这时候调用blockB的同步函数必须执行了blockB才能返回。结果就是: blockA等待dispatch_sync函数返回好继续向下执行,blockB在队列中没法被调用，因为串行队列还没执行完blcokA。所以两者互相等待就死锁了。
-    
-    // 这里用dispatch_sync会锁住当前的queue就无法解释了，因为没发生死锁
+/***
+ 并发队列中的执行过程----在当前的并发队列中执行同步任务不会发生死锁
+    1.blockA任务进入队列queue中,因为是同步任务，要执行blockA任务后才能返回,所以执行blockA。
+    2.在执行blockA任务的过程中blockB任务进入队列queue中，又是同步任务，要执行blockB后才能返回,此时不返回不能执行后续的代码。
+    3.这里正因为是并发队列,可以不用按顺序的执行队列中的任务,也就是说可以不用等待blockA的完成就可以执行blockB。于是执行blockB,之后dispatch_sync函数返回继续执行任务blockA。完美~
+
+ 
+ 串行队列中执行过程----在当前的串行队列中执行同步任务会发生死锁
+    1.blockA任务进入队列queue中,因为是同步任务，要执行blockA任务后才能返回,所以执行blockA。
+    2.在执行blockA任务的过程中blockB任务进入队列queue中，又是同步任务，要执行blockB后才能返回,此时不返回不能执行后续的代码。
+    1.因为是串行队列必须把blockA执行完了才能调用队列中的任务blockB，但是这时候调用blockB的同步函数必须执行了blockB才能返回。结果就是: blockA等待dispatch_sync函数返回好继续向下执行,blockB在队列中没法被调用，因为串行队列还没执行完blcokA。所以两者互相等待就死锁了。
+
+
+ **/
     
 //    2018-07-11 10:42:52.497013+0800 GCD[69181:2046462] 000000000--<NSThread: 0x600000070a80>{number = 1, name = main}
 //    2018-07-11 10:42:52.497143+0800 GCD[69181:2046462] 1111111111--<NSThread: 0x600000070a80>{number = 1, name = main}
@@ -117,23 +233,69 @@
     
 }
 
-// 串行队列
+// 当前队列为主队列--串行队列queueA中执行同步任务
 - (void)test4{
+    /*** 任务0开始**/
+    dispatch_queue_t queueA = dispatch_queue_create("com.yunjifen.serial.queueA", DISPATCH_QUEUE_SERIAL);
     
-    dispatch_queue_t queue = dispatch_queue_create("com.yunjifen.serial", DISPATCH_QUEUE_SERIAL);
+    NSLog(@"00000000--%@",[NSThread currentThread]);
     
     // 没有发生死锁
-        dispatch_sync(queue, ^{
-            sleep(2);
+    dispatch_sync(queueA, ^{
+    //  block1
+        NSLog(@"1111111111--%@",[NSThread currentThread]);
+    });
+
+    NSLog(@"222222222--%@",[NSThread currentThread]);
+    /*** 任务0结束**/
+
+    /**
+        首先 1.当前队列是主队列(属于串行队列) 2.整个test4方法相当于一个任务被加入到了主队列中
+        主队列执行任务0--任务blcok1进入queueA--因为同步任务所以queueA调度线程执行任务block1--执行完成后同步函数返回--主队列继续执行任务0
+     **/
+
+//    2018-07-11 23:43:13.200018+0800 GCD[48507:795658] 00000000--<NSThread: 0x60c000071740>{number = 1, name = main}
+//    2018-07-11 23:43:13.200190+0800 GCD[48507:795658] 1111111111--<NSThread: 0x60c000071740>{number = 1, name = main}
+//    2018-07-11 23:43:13.200367+0800 GCD[48507:795658] 222222222--<NSThread: 0x60c000071740>{number = 1, name = main}
+
+}
+
+// 串行队列
+- (void)test44{
+    
+    dispatch_queue_t queueA = dispatch_queue_create("com.yunjifen.serial.queueA", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_queue_t queueB = dispatch_queue_create("com.yunjifen.serial.queueB", DISPATCH_QUEUE_SERIAL);
+
+    dispatch_sync(queueB, ^{
+        
+        // block0
+        
+        NSLog(@"00000000--%@",[NSThread currentThread]);
+
+        dispatch_sync(queueA, ^{
+            // block1
             NSLog(@"1111111111--%@",[NSThread currentThread]);
         });
-
+        
         NSLog(@"222222222--%@",[NSThread currentThread]);
+        
+    });
     
+    NSLog(@"33333333333--%@",[NSThread currentThread]);
     
+
+    /***   不会发生死锁
+        执行任务block0-任务block1进入queueA-因为是同步任务所以要完成任务block1后函数才能返回(那么此时queueA就调度线程执行任务block1,此时不会死锁是因为没有妨碍queueA调度任务block1的事情)-任务block1执行完成后同步函数返回-继续执行任务block0
+     
+        至于为甚都是在主线程中请看API的说明 （API官方说明：* As an optimization, dispatch_sync() invokes the block on the current thread when possible.)
+     ***/
     
-//    2018-07-11 10:46:46.157353+0800 GCD[69611:2051093] 1111111111--<NSThread: 0x60c00007f340>{number = 1, name = main}
-//    2018-07-11 10:46:46.157490+0800 GCD[69611:2051093] 222222222--<NSThread: 0x60c00007f340>{number = 1, name = main}
+//    2018-07-11 23:22:05.375496+0800 GCD[46454:756803] 00000000--<NSThread: 0x60800007bb40>{number = 1, name = main}
+//    2018-07-11 23:22:05.375661+0800 GCD[46454:756803] 1111111111--<NSThread: 0x60800007bb40>{number = 1, name = main}
+//    2018-07-11 23:22:05.375763+0800 GCD[46454:756803] 222222222--<NSThread: 0x60800007bb40>{number = 1, name = main}
+//    2018-07-11 23:22:05.375884+0800 GCD[46454:756803] 33333333333--<NSThread: 0x60800007bb40>{number = 1, name = main}
+
 
 }
 
@@ -143,26 +305,31 @@
     dispatch_queue_t queue = dispatch_queue_create("com.yunjifen.serial", DISPATCH_QUEUE_SERIAL);
     
     dispatch_sync(queue, ^{
-        
+        // blockA
         NSLog(@"000000000--%@",[NSThread currentThread]);
         
         dispatch_sync(queue, ^{
-            
+            // blockB
             NSLog(@"1111111111--%@",[NSThread currentThread]);
         });
         
-        NSLog(@"222222222--%@",[NSThread currentThread]);
-        
+//        NSLog(@"222222222--%@",[NSThread currentThread]);
+    
     });
     
     NSLog(@"333333333--%@",[NSThread currentThread]);
-    
-    
+    /**
+     串行队列中执行过程----在当前的串行队列中执行同步任务会发生死锁
+     1.blockA任务进入队列queue中,因为是同步任务，要执行blockA任务后才能返回,所以执行blockA。
+     2.在执行blockA任务的过程中blockB任务进入队列queue中，又是同步任务，要执行blockB后才能返回,此时不返回不能执行后续的代码。
+     3.因为是串行队列必须把blockA执行完了才能调用队列中的blockB任务，但是这时候调用blockB的同步函数必须执行了blockB才能返回。结果就是: blockA等待dispatch_sync函数返回好继续向下执行,blockB在队列中没法被调用，因为串行队列还没执行完blcokA。所以两者互相等待就死锁了。
+     **/
+
+
 //    2018-07-11 10:48:20.130852+0800 GCD[69785:2053126] 000000000--<NSThread: 0x604000072e00>{number = 1, name = main}
 //    发生了死锁
 
 }
-
 
 
 - (void)test6{
@@ -172,35 +339,78 @@
     dispatch_async(queue, ^{
         
         NSLog(@"000000000--%@",[NSThread currentThread]);
-
+        
         dispatch_sync(queue, ^{
             
             NSLog(@"1111111111--%@",[NSThread currentThread]);
-
+            
             dispatch_sync(queue, ^{
                 
                 NSLog(@"222222222--%@",[NSThread currentThread]);
             });
             
             NSLog(@"3333333333--%@",[NSThread currentThread]);
-
+            
         });
         
         NSLog(@"44444444444--%@",[NSThread currentThread]);
-
+        
     });
     
     NSLog(@"5555555555--%@",[NSThread currentThread]);
     
-//    2018-07-11 11:00:01.367774+0800 GCD[71059:2066624] 5555555555--<NSThread: 0x604000076e00>{number = 1, name = main}
-//    2018-07-11 11:00:01.367789+0800 GCD[71059:2066746] 000000000--<NSThread: 0x608000272440>{number = 4, name = (null)}
-//    发生了死锁
+    //    2018-07-11 11:00:01.367774+0800 GCD[71059:2066624] 5555555555--<NSThread: 0x604000076e00>{number = 1, name = main}
+    //    2018-07-11 11:00:01.367789+0800 GCD[71059:2066746] 000000000--<NSThread: 0x608000272440>{number = 4, name = (null)}
+    //    发生了死锁
     
 }
+/**************************** 为什么死锁的分割线 **********************/
 
 
 
-// MARK:<测试dispatch_async在并发队列、串行队列中运行场景>
+// MARK:<dispatch_async()在并发队列、串行队列中运行场景>
+
+/*!
+ * @function dispatch_async
+ *
+ * @abstract
+ * Submits a block for asynchronous execution on a dispatch queue.
+ *
+ * @discussion
+ * The dispatch_async() function is the fundamental mechanism for submitting
+ * blocks to a dispatch queue.
+    dispatch_async() 函数是向队列提交blcok(任务)最基本的机制
+ *
+ * Calls to dispatch_async() always return immediately after the block has
+ * been submitted, and never wait for the block to be invoked.
+    dispatch_async()函数将blcok(任务)提交到队列后，会立刻返回,从不会等待任务被调用
+ 
+ *
+ * The target queue determines whether the block will be invoked serially or
+ * concurrently with respect to other blocks submitted to that same queue.
+ * Serial queues are processed concurrently with respect to each other.
+ *
+   队列的类型决定调用block(任务)是串行的还是并发的(可能有多个blocks进入同一个队列中)
+   多个串行队列会被并发的进行处理？？？
+ 
+ * @param queue
+ * The target dispatch queue to which the block is submitted.
+ * The system will hold a reference on the target queue until the block
+ * has finished.
+   系统会引用目标队列,直到任务完成
+ 
+ * The result of passing NULL in this parameter is undefined.
+ *
+ * @param block
+ * The block to submit to the target dispatch queue. This function performs
+ * Block_copy() and Block_release() on behalf of callers.
+    dispatch_async()函数会为调用者对block(任务)进行Block_copy()和Block_release()
+ 
+ * The result of passing NULL in this parameter is undefined.
+ */
+
+
+
 // 并发队列
 -(void)test7{
     
@@ -561,8 +771,6 @@
     //    2018-07-11 16:34:22.992031+0800 GCD[2629:1227559] queueAAAAA3333--<NSThread: 0x1c00763c0>{number = 3, name = (null)}
     //    2018-07-11 16:34:22.992330+0800 GCD[2629:1227559] queueAAAAA4444--<NSThread: 0x1c00763c0>{number = 3, name = (null)}
     //    2018-07-11 16:34:22.992350+0800 GCD[2629:1227558] queueBBBBB6666--<NSThread: 0x1c0075280>{number = 5, name = (null)}
-    
-    
     
 }
 
